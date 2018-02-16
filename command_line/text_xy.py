a3356d328fd69538c7e32664b421b0de6f24a0b4
graeme-winter <graeme.winter@gmail.com>
2018-02-16 08:46:30 +0000

    Jiffy to dump x, y positions to text files

---
+++ command_line/text_xy.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import, division, print_function
+
+import iotbx.phil
+
+phil_scope = iotbx.phil.parse("""
+  close = 10.0
+    .type = float
+  alignments = rotations.json
+    .type = path
+  prefix = stars
+    .type = str
+""", process_includes=False)
+
+def run(args):
+  from dials.util.options import OptionParser
+  import libtbx.load_env
+
+  usage = "%s [options] stars.pickle alignments=rotations.json" % (
+    libtbx.env.dispatcher_name)
+
+  parser = OptionParser(
+    usage=usage,
+    phil=phil_scope)
+
+  params, options, args = parser.parse_args(show_diff_phil=True,
+                                            return_unhandled=True)
+
+  from dials.array_family import flex
+  import cPickle as pickle
+  import json
+
+  Rtds = json.load(open(params.alignments))
+  stars = None
+
+  for arg in args:
+    tmp_stars = pickle.load(open(arg))
+    if not stars:
+      stars = tmp_stars
+    else:
+      stars.extend(tmp_stars)
+
+  x, y, z = stars['xyzobs.px.value'].parts()
+
+  zs = list(set(z))
+  zs.sort()
+
+  assert len(Rtds) == len(zs)
+
+  results = { }
+
+  from scitbx import matrix
+
+  for _z, alignment in zip(zs, Rtds):
+    _x = x.select(z == _z)
+    _y = y.select(z == _z)
+    R, t = alignment['R'], alignment['t']
+    _R = matrix.sqr(R)
+    _xy = [_R * matrix.col((__x, __y)) + matrix.col(t) for __x, __y in zip(_x, _y)]
+    with open('%s_%02d.dat' % (params.prefix, int(_z)), 'w') as f:
+      for __xy in _xy:
+        f.write('%f %f\n' % __xy.elems)
+
+if __name__ == '__main__':
+  import sys
+  run(sys.argv[1:])
