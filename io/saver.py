214c3be1e3d3738e29df2d09dc20d0f9fcf88310
graeme-winter <graeme.winter@gmail.com>
2018-02-16 06:34:42 +0000

    Prevent saturated pixels

--- io/saver.py
+++ io/saver.py
@@ -6,8 +6,18 @@ def save_image(filename, r, g, b):
   import numpy
   from PIL import Image
 
-  rn = Image.fromarray(numpy.uint8(r.iround().as_numpy_array()))
-  gn = Image.fromarray(numpy.uint8(g.iround().as_numpy_array()))
-  bn = Image.fromarray(numpy.uint8(b.iround().as_numpy_array()))
+  ri = r.iround()
+  gi = g.iround()
+  bi = b.iround()
+
+  # prevent saturation of pixels > 0xff
+
+  ri.set_selected(ri > 255, 255)
+  gi.set_selected(gi > 255, 255)
+  bi.set_selected(bi > 255, 255)
+
+  rn = Image.fromarray(numpy.uint8(ri.as_numpy_array()))
+  gn = Image.fromarray(numpy.uint8(gi.as_numpy_array()))
+  bn = Image.fromarray(numpy.uint8(bi.as_numpy_array()))
 
   Image.merge('RGB', (rn, gn, bn)).save(filename)
