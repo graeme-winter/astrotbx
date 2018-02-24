49ca67156ba7bfea6f3dedb4d73fb0285c2e48dd
graeme-winter <graeme.winter@gmail.com>
2018-02-24 21:40:45 +0000

    Annoying; turns out sign inversion here wrong

--- algorithms/image_align.py
+++ algorithms/image_align.py
@@ -8,6 +8,6 @@ def rotate_translate_array(image, R, t):
   # OK, this is defined in a very weird coordinate system i.e. the
   # resulting system, and in C-array settings => slow, fast for
   # translations...
-  matrix = numpy.array([[R[0], -R[1], -t[1]], [-R[2], R[3], -t[0]]])
+  matrix = numpy.array([[R[0], R[1], -t[1]], [R[2], R[3], -t[0]]])
 
   return flex.double(affine_transform(image.as_numpy_array(), matrix))
