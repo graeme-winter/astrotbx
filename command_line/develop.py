948cc7010d5dc9ae2cd39c2e4c1b9aea8a4e7988
graeme-winter <graeme.winter@gmail.com>
2018-03-04 21:21:55 +0000

    From pickle file, develop png #todo add adobe etc, colour spaces

---
+++ command_line/develop.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+
+import iotbx.phil
+
+phil_scope = iotbx.phil.parse("""
+  scale = 1.0
+    .type = float
+  output = stacked.png
+    .type = path
+  data = stacked.pickle
+    .type = path
+""", process_includes=False)
+
+def run(args):
+  from dials.util.options import OptionParser
+  import libtbx.load_env
+  import json
+
+  usage = "%s [options] DSC03206.jpg" % (
+    libtbx.env.dispatcher_name)
+
+  parser = OptionParser(
+    usage=usage,
+    phil=phil_scope)
+
+  params, options, args = parser.parse_args(show_diff_phil=True,
+                                            return_unhandled=True)
+
+  import cPickle as pickle
+  from dials.array_family import flex
+
+  with fin as open(params.data):
+    sum_image_r, sum_image_g, sum_image_b = pickle.load(fin)
+
+  sum_image_r *= params.scale
+  sum_image_g *= params.scale
+  sum_image_b *= params.scale
+
+  # output the image
+
+  from astrotbx.input_output.saver import save_image
+  save_image(params.output, sum_image_r, sum_image_g, sum_image_b)
+
+if __name__ == '__main__':
+  import sys
+  run(sys.argv[1:])
